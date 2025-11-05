7b4a100f8d3e28763ec5ea7a33cf2feca27837f5
RhysGEvans <88195825+RhysEvan@users.noreply.github.com>
2025-11-05 11:53:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ wx_dep = "wxPython>=4.2.3 ; sys_platform!='linux'"
 
 setup(
     name="CTPv",
-    version="0.2.3-2",
+    version="0.2.3-3",
     description="A toolbox of camera, Plücker and transformation utilities",
     author="Rhys Evans & Seppe Sels",
     license="MIT",
